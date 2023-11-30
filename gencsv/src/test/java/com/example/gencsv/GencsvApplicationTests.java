6110fc38ac300bcb7b4865c67bda474f6ae34838
Vuong Tien <vuongtien2942000@gmail.com>
2023-11-30 18:05:12 +0700

    fhshfjsdhf

---
+++ gencsv/src/test/java/com/example/gencsv/GencsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gencsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GencsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/gencsv/GencsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.gencsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GencsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GencsvApplication.class, args);
+	}
+
+}
